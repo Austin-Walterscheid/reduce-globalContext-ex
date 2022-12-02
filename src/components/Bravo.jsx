2f9307fd3d598d040e84bbc48d4f393fe6759d8c
Austin Walterscheid <aus.10walterscheid@gmail.com>
2022-12-02 10:16:27 -0700

    example

---
+++ src/components/Bravo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Charlie from './Charlie'
+
+const Bravo = () => {
+    return (
+        <div>
+           <h1>Bravo</h1>
+           <Charlie/>
+            </div>
+        
+    )
+}
+
+export default Bravo
---
+++ src/components/Charlie.jsx
@@ -0,0 +1,14 @@
+import React, {useContext} from 'react'
+import GlobalContext from '../store/GlobalContext'
+
+
+
+const Charlie = () => {
+    const {state, dispatch} = useContext
+    (GlobalContext)
+    return (
+        <div>{state.name}</div>
+    )
+}
+
+export default Charlie
